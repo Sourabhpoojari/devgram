ca0c549f588818161e1ced2f90ebce15a58dcc7a
Sourabhpoojari <sourabhpoojari674@gmail.com>
2020-08-07 22:50:50 +0530

    added mongodb connection

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express'),
 app = express();
+const connectDB = require('./config/db');
+
+
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.get('/',(req,res)=>res.send('Api running'));
