35816ca15729dbbfa20a4ca1938cc73f54d77c82
Sourabhpoojari <sourabhpoojari674@gmail.com>
2020-08-22 22:03:12 +0530

    added github route

--- controllers/profile-controllers.js
+++ controllers/profile-controllers.js
@@ -1,4 +1,5 @@
 const Profile = require('../models/profile'),
+config = require('config'),
     User = require('../models/user');
     const {check, validationResult} = require('express-validator');
 
@@ -242,6 +243,30 @@ const deleteEducation = async (req,res,next)=>{
     }
 };
 
+// @route GET profile/github/:username
+// @desc get github repos
+// @access Public
+const getRepos = async (req,res,next)=>{
+    try {
+        const options = {
+            uri : `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get(githubClientId)}&client_secret=${config.get(githubSecret)}`,
+            method : `GET`,
+            headers : {'user-agent':'node.js'}
+        }
+        request(options,(error,response,body)=>{
+            if (error) {
+                console.error(error);
+            }
+            if(response.statusCode !== 200){
+                res.status(404).json({msg:'No Github profile found'});
+            }
+            res.json(JSON.parse(body))
+        })
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+};
 
 
 exports.getProfile = getProfile;
