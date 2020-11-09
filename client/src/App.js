5045478290e0a4d9bf8e89fa6d2353140c19c914
Sourabhpoojari <sourabhpoojari674@gmail.com>
2020-11-09 14:15:08 +0530

    Private routing

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import './App.css';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/Routings/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -31,6 +33,7 @@ const App = ()=> {
           <Switch>
             <Route exact path='/login' component={Login}/>
             <Route exact path='/register' component={Register}/>
+            <PrivateRoute  exact path='/dashboard' component={ Dashboard }/>
           </Switch>
       </section>
     </Fragment>
